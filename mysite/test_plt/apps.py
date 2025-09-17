62f505bb5b7f9228687cd4a89be29793c947abe0
Flamindle <wangyaxv@163.com>
2025-09-17 19:18:34 +0800

    init

---
+++ mysite/test_plt/apps.py
@@ -0,0 +1,10 @@
+#encoding: utf-8
+from django.apps import AppConfig
+
+
+class TestPltConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    #给程序看的
+    name = 'test_plt'
+    #给人看的
+    verbose_name='测试平台'

faa33a5ba9e2d87b085bac0a2456f931e480aeca
Flamindle <wangyaxv@163.com>
2025-09-18 19:56:49 +0800

    1.前端修改+筛选

--- mysite/test_plt/admin.py
+++ mysite/test_plt/admin.py
@@ -1,10 +1,21 @@
 # -*- coding: utf-8 -*-
 from django.contrib import admin
+from django.contrib.admin import ModelAdmin
+
 from .models import Project
+
+
+
+class ProjectAdmin(ModelAdmin):
+    list_display = ['id','name','version','type','status','create_user','create_time','update_time']
+    list_display_links = ['name','version']
+    list_filter = ['create_time','type','create_user']
+    search_fields = ['name','version']
+
 
 # Register your models here.
 # admin.site.register(Project, ProjectAdmin)
-admin.site.register(Project)
+admin.site.register(Project, ProjectAdmin)
 
 admin.site.site_header = "自动化测试后台管理系统"
 admin.site.site_title = "Jacky自动化平台自研系统"
